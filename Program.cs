d1a6db83d1a2050ac2805cdd98c5274d7166f7a3
techLinky <gcebile.sne@gmail.com>
2023-09-13 15:28:43 +0200

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _9.Arrays_1_dimensional
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            //declaration of arrays
+            int[] LuckyNumbers = { 1, 2, 3, 4, 5, 6, 7, 8 };
+
+            Console.WriteLine(LuckyNumbers.Length);
+            Console.WriteLine(LuckyNumbers[0]);
+
+            string[] friends = new string[5];
+            friends[0] = "Aphiwe";
+            friends[1] = "Mukelo";
+            friends[2] = "Luyandah";
+            friends[3] = "Ayaan";
+
+            Console.WriteLine(friends[3]);
+            
+            //freeze the console
+            Console.ReadLine();
+        }
+    }
+}
